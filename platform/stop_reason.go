f3659d9c2629f46b545630646172f77b5c7802a8
Alexander Jung <alex@unikraft.cloud>
2025-06-30 13:53:32 -0700

    feat: Add utility stop reason methods
    Signed-off-by: Alexander Jung <alex@unikraft.cloud>

---
+++ platform/stop_reason.go
@@ -0,0 +1,318 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2023, Unikraft GmbH.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
+package platform
+
+import (
+	"fmt"
+	"math"
+	"strings"
+	"syscall"
+	"time"
+)
+
+// Stop code of the kernel.  This value encodes multiple details about the stop
+// irrespective of the application.
+//
+// MSB                                                     LSB
+// ┌──────────────┬──────────┬──────────┬───────────┬────────┐
+// │ 31 ────── 24 │ 23 ── 16 │    15    │ 14 ──── 8 │ 7 ── 0 │
+// ├──────────────┼──────────┼──────────┼───────────┼────────┤
+// │ reserved[^1] │ errno    │ shutdown │ initlevel │ reason │
+// └──────────────┴──────────┴──────────┴───────────┴────────┘
+//
+// [^1]:      Reserved for future use.
+// errno:     The application errno, using Linux's errno.h values.  (Optional,
+//            can be 0.)
+// shutdown:  Whether the shutdown originated from the inittable (0) or from the
+//            termtable (1).
+// initlevel: The initlevel at the time of the stop.
+// reason:    The reason for the stop. See `StopCodeReason`.
+
+const (
+	StopCodeMaskErrno     uint32 = 0xFF0000
+	StopCodeMaskShutdown  uint32 = 0x008000
+	StopCodeMaskInitLevel uint32 = 0x007F00
+	StopCodeMaskReason    uint32 = 0x0000FF
+)
+
+// StopCodeErrno returns the application errno, using Linux's errno.h values.
+func (item *Instance) StopCodeErrno() uint8 {
+	stopCode, ok := item.StopCode.Get()
+	if !ok {
+		return 0
+	}
+
+	return uint8((stopCode & StopCodeMaskErrno) >> 16)
+}
+
+// StopCodeShutdownTable returns whether the stop originated from the inittable
+// (0) or from the termtable (1).
+func (item *Instance) StopCodeShutdownTable() uint8 {
+	stopCode, ok := item.StopCode.Get()
+	if !ok {
+		return 0
+	}
+
+	return uint8((stopCode & StopCodeMaskShutdown) >> 15)
+}
+
+// StopCodeInitLevel returns the initlevel at the time of the stop.
+func (item *Instance) StopCodeInitLevel() uint8 {
+	stopCode, ok := item.StopCode.Get()
+	if !ok {
+		return 0
+	}
+
+	return uint8((stopCode & StopCodeMaskInitLevel) >> 8)
+}
+
+const (
+	// 0 - Success
+	StopCodeReasonOK uint8 = iota
+
+	// 1 - Explicit crash (bugon/assert/crash/unhandled breakpoint)
+	StopCodeReasonEXP
+
+	// 2 - Arithmetic error
+	StopCodeReasonMATH
+
+	// 3 - Invalid CPU instruction or instruction error (e.g., operand alignment)
+	StopCodeReasonINVLOP
+
+	// 4 - Page fault - see errno (out of mem, EFAULT)
+	StopCodeReasonPGFAULT
+
+	// 5 - Segmentation fault
+	StopCodeReasonSEGFAULT
+
+	// 6 - Hardware error, NMI, alignment checks
+	StopCodeReasonHWERR
+
+	// 7 - Security violation, control protection (MTE, shadow stacks, PKU?)
+	StopCodeReasonSECERR
+)
+
+// StopCodeReason returns all the reasons for the stop.
+func StopCodeReasons() []string {
+	return []string{
+		"OK",
+		"EXP",
+		"MATH",
+		"INVLOP",
+		"PGFAULT",
+		"SEGFAULT",
+		"HWERR",
+		"SECERR",
+	}
+}
+
+// StopCodeReason provides the identity value for the reason for the stop.
+func (item *Instance) StopCodeReason() uint8 {
+	stopCode, ok := item.StopCode.Get()
+	if !ok {
+		return StopCodeReasonOK
+	}
+
+	return uint8(stopCode & StopCodeMaskReason)
+}
+
+const (
+	StopReasonKernel      uint32 = 1 << iota // 0b00001
+	StopReasonApplication                    // 0b00010
+	StopReasonPlatform                       // 0b00100
+	StopReasonUser                           // 0b01000
+	StopReasonForced                         // 0b10000
+)
+
+// DescribeStopOrigin provides a human-readable interpretation of the stop
+// reason.
+func (item Instance) DescribeStopOrigin() string {
+	stopReason, ok := item.StopReason.Get()
+	if !ok || stopReason == 0 {
+		return "unknown"
+	}
+
+	var ret strings.Builder
+
+	if stopReason&StopReasonForced != 0 {
+		ret.WriteString("force ")
+	}
+
+	ret.WriteString("initiated by ")
+
+	switch true {
+	case stopReason&StopReasonPlatform == StopReasonPlatform && stopReason&StopReasonUser != StopReasonUser:
+		ret.WriteString("platform")
+	case stopReason&StopReasonUser == StopReasonUser:
+		ret.WriteString("user")
+	case stopReason&StopReasonApplication == StopReasonApplication:
+		ret.WriteString("app")
+	case stopReason&StopReasonKernel == StopReasonKernel:
+		ret.WriteString("kernel")
+	}
+
+	return ret.String()
+}
+
+// StopOriginCode provides a human-readable interpretation of the stop reason in
+// the form of a short-code.
+func (item Instance) StopOriginCode() string {
+	stopReason, ok := item.StopReason.Get()
+	if !ok || stopReason == 0 {
+		return "-----"
+	}
+
+	var ret strings.Builder
+
+	if stopReason&StopReasonForced == StopReasonForced {
+		ret.WriteString("f")
+	} else {
+		ret.WriteString("-")
+	}
+	if stopReason&StopReasonUser == StopReasonUser {
+		ret.WriteString("u")
+	} else {
+		ret.WriteString("-")
+	}
+	if stopReason&StopReasonPlatform == StopReasonPlatform {
+		ret.WriteString("p")
+	} else {
+		ret.WriteString("-")
+	}
+	if stopReason&StopReasonApplication == StopReasonApplication {
+		ret.WriteString("a")
+	} else {
+		ret.WriteString("-")
+	}
+	if stopReason&StopReasonKernel == StopReasonKernel {
+		ret.WriteString("k")
+	} else {
+		ret.WriteString("-")
+	}
+
+	return ret.String()
+}
+
+// DescribeStopReason provides a human-readable description of the stop reason.
+func (item Instance) DescribeStopReason() string {
+	stopReason, ok := item.StopReason.Get()
+	if !ok || stopReason == 0 {
+		return ""
+	}
+
+	var ret strings.Builder
+
+	switch true {
+	case item.StopCodeReason() == StopCodeReasonOK:
+		ret.WriteString("shutdown")
+	case item.StopCodeReason() == StopCodeReasonEXP:
+		ret.WriteString("assertion error")
+	case item.StopCodeReason() == StopCodeReasonPGFAULT && item.StopCodeErrno() == 0xc:
+		ret.WriteString("out of memory")
+	case item.StopCodeReason() == StopCodeReasonPGFAULT && (item.StopCodeErrno() == 0xe || item.StopCodeErrno() == 0x1):
+		ret.WriteString("illegal memory access")
+	case item.StopCodeReason() == StopCodeReasonSEGFAULT:
+		ret.WriteString("segmentation fault")
+	case item.StopCodeReason() == StopCodeReasonPGFAULT:
+		ret.WriteString("page fault")
+	case item.StopCodeReason() == StopCodeReasonMATH:
+		ret.WriteString("arithmetic error")
+	case item.StopCodeReason() == StopCodeReasonINVLOP:
+		ret.WriteString("instruction error")
+	case item.StopCodeReason() == StopCodeReasonHWERR:
+		ret.WriteString("hardware error")
+	case item.StopCodeReason() == StopCodeReasonSECERR:
+		ret.WriteString("security violation")
+	default:
+		ret.WriteString("unexpected error")
+	}
+
+	return ret.String()
+}
+
+// StopReasonCode returns a human-readable short-code representation of the stop
+// reason.
+func (item Instance) StopReasonCode() string {
+	stopReason, ok := item.StopReason.Get()
+	if !ok || stopReason == 0 {
+		return ""
+	}
+
+	var ret strings.Builder
+
+	if item.StopCodeShutdownTable() == 0 {
+		ret.WriteString("i")
+	} else {
+		ret.WriteString("t")
+	}
+
+	ret.WriteString(fmt.Sprintf("%d", item.StopCodeInitLevel()))
+
+	ret.WriteString(" ")
+
+	ret.WriteString(StopCodeReasons()[item.StopCodeReason()])
+
+	if item.StopCodeErrno() != 0 {
+		ret.WriteString(" ")
+		errno, ok := ErrnoNames()[syscall.Errno(item.StopCodeErrno())]
+		if ok {
+			ret.WriteString(errno)
+		} else {
+			ret.WriteString(fmt.Sprintf("%d", item.StopCodeErrno()))
+		}
+	}
+
+	return ret.String()
+}
+
+// DescribeStatus returns a human-readable description of the instance's status.
+func (item Instance) DescribeStatus() string {
+	switch item.State.Value {
+	case InstanceStateRunning:
+		dur := time.Since(item.StartedAt.Value)
+		days := int64(dur.Hours() / 24)
+		hours := int64(math.Mod(dur.Hours(), 24))
+		minutes := int64(math.Mod(dur.Minutes(), 60))
+		seconds := int64(math.Mod(dur.Seconds(), 60))
+
+		chunks := []struct {
+			singularName string
+			amount       int64
+		}{
+			{"day", days},
+			{"hr", hours},
+			{"min", minutes},
+			{"sec", seconds},
+		}
+
+		parts := []string{}
+
+		for i, chunk := range chunks {
+			if len(parts) > 0 && i+1 == len(chunks) { // Skip secs if greater than 1m
+				continue
+			}
+			switch chunk.amount {
+			case 0:
+				continue
+			case 1:
+				parts = append(parts, fmt.Sprintf("%d%s", chunk.amount, chunk.singularName))
+			default:
+				parts = append(parts, fmt.Sprintf("%d%ss", chunk.amount, chunk.singularName))
+			}
+		}
+
+		return fmt.Sprintf("since %s", strings.Join(parts, " "))
+	case InstanceStateStopped:
+		reason := item.DescribeStopReason()
+		if reason == "shutdown" {
+			return ""
+		}
+
+		return reason
+	default:
+		return string(item.State.Value)
+	}
+}
