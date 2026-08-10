0bb1cc1a1402fcba98ed72dc1950500acd4563f4
Justin Chadwell <justin@unikraft.com>
2026-08-10 16:56:32 +0100

    feat(httpclient): Support client options
    Then we can replace the CLI specialized clients with these.
    
    Signed-off-by: Justin Chadwell <justin@unikraft.com>

--- controlplane/client.gen.go
+++ controlplane/client.gen.go
@@ -271,7 +271,7 @@ func NewClient(copts ...ClientOption) Client {
 	}
 
 	if options.AllowInsecure() && options.HTTPClient() == nil {
-		options.SetHTTPClient(httpclient.NewInsecureHTTPClient())
+		options.SetHTTPClient(httpclient.NewHTTPClient(httpclient.WithInsecure()))
 	}
 
 	if options.HTTPClient() == nil {
