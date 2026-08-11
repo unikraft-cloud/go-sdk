f11e1b8142dbaf841b78efb1fee69c94a47574de
Justin Chadwell <justin@unikraft.com>
2026-08-11 16:33:13 +0100

    chore: Remove the Sandbox API client
    The Sandbox API is being removed upstream. Drop the generated sandbox
    package and the Makefile/workflow wiring that generated and synced it.
    
    Signed-off-by: Justin Chadwell <justin@unikraft.com>

--- sandbox/generate.go
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2026, Unikraft GmbH.
-// Licensed under the BSD-3-Clause License (the "License").
-// You may not use this file except in compliance with the License.
-
-package sandbox
-
-//go:generate go run -C ../tools/openapi-gen unikraft.com/x/tools/openapi-gen -i ../../sandbox.yaml -o ../../sandbox -v package=sandbox -t ./templates
